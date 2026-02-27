f5d2002ca39fa4758164619fbd8126e7a299e066
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 01:42:10 +0200

    DbContexts

--- ECommerce.Persistence/Data/DbContexts/StoreDbContext.cs
+++ ECommerce.Persistence/Data/DbContexts/StoreDbContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECommerce.Domain.Entities.IdentityModule;
 using ECommerce.Domain.Entities.ProductModule;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Persistence.Data.DbContexts
 {
+    #region DbContexts
     public class StoreDbContext : DbContext
     {
         public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
@@ -19,4 +23,24 @@ namespace ECommerce.Persistence.Data.DbContexts
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
     }
+    public class StoreIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public StoreIdentityDbContext(DbContextOptions<StoreIdentityDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Address>().ToTable("Addresses");
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<ApplicationUser>()
+                   .HasOne(a => a.Address)
+                   .WithOne(a => a.ApplicationUser)
+                   .HasForeignKey<Address>(a => a.UserId);
+        }
+    }
+    #endregion
 }
