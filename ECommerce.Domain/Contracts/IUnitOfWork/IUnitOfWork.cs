710a2f9d510c27a8135429943fd7dd8cc30c0891
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 01:39:16 +0200

    SpecificationDesignPattern

--- ECommerce.Domain/Contracts/IUnitOfWork/IUnitOfWork.cs
+++ ECommerce.Domain/Contracts/IUnitOfWork/IUnitOfWork.cs
@@ -14,28 +14,6 @@ namespace ECommerce.Domain.Contracts.IUnitOfWork
         Task<int> SaveChangesAsync();
         IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
     }
-    public class UnitOfWork : IUnitOfWork
-    {
-        private readonly StoreDbContext _dbContext;
-        private readonly Dictionary<Type, object> _repositories = [];
-
-        public UnitOfWork(StoreDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
-        {
-            var EntityType = typeof(TEntity);
-            if (_repositories.TryGetValue(EntityType, out object? repository))
-            {
-                return (IGenericRepository<TEntity, TKey>)repository;
-            }
-            var newRepo = new GenericRepository<TEntity, TKey>(_dbContext);
-            _repositories[EntityType] = newRepo;
-            return newRepo;
-        }
-
-        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
-    }
+    
     #endregion
 }
--- ECommerce.Domain/Contracts/Specification/ISpecifications.cs
+++ ECommerce.Domain/Contracts/Specification/ISpecifications.cs
@@ -5,9 +5,11 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Domain.Contracts.Specification
 {
+    #region SpecificationDesignPattern
     public interface ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
         public ICollection<Expression<Func<TEntity, object>>> IncludeExpressions { get; }
@@ -18,4 +20,78 @@ namespace ECommerce.Domain.Contracts.Specification
         public int skip { get; }
         public bool IsPaginated { get; }
     }
+    internal abstract class BaseSpecification<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public Expression<Func<TEntity, bool>> Criteria { get; }
+        public BaseSpecification(Expression<Func<TEntity, bool>> CriteriaExpression)
+        {
+            Criteria = CriteriaExpression;
+        }
+
+        public Expression<Func<TEntity, object>>? SortAsc { get; private set; }
+        protected void AddSortAsc(Expression<Func<TEntity, object>> sortAscExp)
+        {
+            SortAsc = sortAscExp;
+        }
+
+        public Expression<Func<TEntity, object>>? SortDesc { get; private set; }
+        protected void AddSortDesc(Expression<Func<TEntity, object>> sortDescExp)
+        {
+            SortDesc = sortDescExp;
+        }
+
+        public ICollection<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = [];
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExp)
+        {
+            IncludeExpressions.Add(includeExp);
+        }
+
+        public int take { get; private set; }
+        public int skip { get; private set; }
+        public bool IsPaginated { get; private set; }
+        protected void ApplyPagination(int PageIndex, int PadeSize)
+        {
+            IsPaginated = true;
+            take = PadeSize;
+            skip = (PageIndex - 1) * PadeSize;
+        }
+    }
+    internal static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> CreateQuery<TEntity, TKey>(IQueryable<TEntity> EntryPoint,
+            ISpecifications<TEntity, TKey> specifications) where TEntity : BaseEntity<TKey>
+        {
+            var query = EntryPoint;
+            if (specifications is not null)
+            {
+                if (specifications.Criteria is not null)
+                {
+                    query = query.Where(specifications.Criteria);
+                }
+
+                if (specifications.IncludeExpressions is not null && specifications.IncludeExpressions.Any())
+                {
+                    query = specifications.IncludeExpressions
+                        .Aggregate(query, (currentquery, includeExp) => currentquery.Include(includeExp));
+                }
+
+                if (specifications.SortAsc is not null)
+                {
+                    query = query.OrderBy(specifications.SortAsc);
+                }
+
+                if (specifications.SortDesc is not null)
+                {
+                    query = query.OrderByDescending(specifications.SortDesc);
+                }
+
+                if (specifications.IsPaginated)
+                {
+                    query = query.Skip(specifications.skip).Take(specifications.take);
+                }
+            }
+            return query;
+        }
+    }
+    #endregion
 }
