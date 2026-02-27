8e2652e5eaefb46aaa4d3490e5ff542b4fe1c387
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 01:44:43 +0200

    AuthenticationService

--- ECommerce.Persistence/Data/DbContexts/StoreDbContext.cs
+++ ECommerce.Persistence/Data/DbContexts/StoreDbContext.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ECommerce.Domain.Entities.IdentityModule;
 using ECommerce.Domain.Entities.ProductModule;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Persistence.Data.DbContexts
@@ -22,25 +19,6 @@ namespace ECommerce.Persistence.Data.DbContexts
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
-    }
-    public class StoreIdentityDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public StoreIdentityDbContext(DbContextOptions<StoreIdentityDbContext> options) : base(options)
-        {
-
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.Entity<Address>().ToTable("Addresses");
-            builder.Entity<ApplicationUser>().ToTable("Users");
-            builder.Entity<IdentityRole>().ToTable("Roles");
-            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
-            builder.Entity<ApplicationUser>()
-                   .HasOne(a => a.Address)
-                   .WithOne(a => a.ApplicationUser)
-                   .HasForeignKey<Address>(a => a.UserId);
-        }
     }
     #endregion
 }
--- ECommerce.Services Abstraction/IServices/IAuthenticationService.cs
+++ ECommerce.Services Abstraction/IServices/IAuthenticationService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerce.Shared.Dtos.IdentityDtos;
@@ -8,6 +9,7 @@ using ECommerce.Shared.ResultPattern;
 
 namespace ECommerce.Services_Abstraction.IServices
 {
+    #region AuthenticationService
     public interface IAuthenticationService
     {
         public Task<Result<UserDto>> LoginAsync(LoginDto loginDto);
@@ -15,4 +17,90 @@ namespace ECommerce.Services_Abstraction.IServices
         public Task<bool> CheckEmailAsync(string email);
         public Task<Result<UserDto>> GetUserByEmailAsync(string email);
     }
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationService(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<bool> CheckEmailAsync(string email)
+        {
+            var User = await _userManager.FindByEmailAsync(email);
+            return User is not null;
+        }
+
+        public async Task<Result<UserDto>> GetUserByEmailAsync(string email)
+        {
+            var User = await _userManager.FindByEmailAsync(email);
+            if (User is null)
+                return Error.NotFound("User.NotFound", $"User With Email: {email} Not Found");
+            return new UserDto(User.Email!, User.DisplayName, await GenerateToken(User));
+        }
+
+        public async Task<Result<UserDto>> LoginAsync(LoginDto loginDto)
+        {
+            var User = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (User is null)
+                return Error.InvalidCredentials("User.InvalidCredentials");
+            var IsPasswordValid = await _userManager.CheckPasswordAsync(User, loginDto.Password);
+            if (!IsPasswordValid)
+                return Error.InvalidCredentials("User.InvalidCredentials");
+            var Token = await GenerateToken(User);
+            return new UserDto
+            (
+                User.Email!,
+                User.DisplayName,
+                Token
+            );
+        }
+
+        public async Task<Result<UserDto>> RegisterAsync(RegisterDto registerDto)
+        {
+            var User = new ApplicationUser()
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber
+            };
+            var IdentityResult = await _userManager.CreateAsync(User, registerDto.Password);
+            if (IdentityResult.Succeeded)
+            {
+                var Token = await GenerateToken(User);
+                return new UserDto(User.Email!, User.DisplayName, Token);
+            }
+            return Result<UserDto>.Fail(IdentityResult.Errors.Select(E => Error.Validation(E.Code, E.Description)).ToList());
+        }
+
+        private async Task<string> GenerateToken(ApplicationUser user)
+        {
+            var Claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Email,user.Email!),
+                new Claim(JwtRegisteredClaimNames.Name,user.UserName!)
+            };
+            var Roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in Roles)
+            {
+                Claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var SecretKey = _configuration["JwtOptions:SecretKey"];
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var Creds = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+            var Token = new JwtSecurityToken(
+                issuer: _configuration["JwtOptions:Issuer"],
+                audience: _configuration["JwtOptions:Audience"],
+                expires: DateTime.UtcNow.AddHours(1),
+                claims: Claims,
+                signingCredentials: Creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(Token);
+        }
+    }
+    #endregion
 }
