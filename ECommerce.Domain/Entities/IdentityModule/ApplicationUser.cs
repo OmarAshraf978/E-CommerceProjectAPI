d5c32a37a38ca5404ccd18675d45f054a5b3fe9f
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 01:31:09 +0200

    IdentityModule

--- ECommerce.Domain/Entities/IdentityModule/ApplicationUser.cs
+++ ECommerce.Domain/Entities/IdentityModule/ApplicationUser.cs
@@ -8,9 +8,22 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ECommerce.Domain.Entities.IdentityModule
 {
+    #region IdentityModule
     public class ApplicationUser : IdentityUser
     {
         public string DisplayName { get; set; } = default!;
         public Address? Address { get; set; } 
     }
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; } = default!;
+        public string Street { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public ApplicationUser ApplicationUser { get; set; } = default!;
+        public string UserId { get; set; } = default!;
+    }
+    #endregion
 }
