13479aff3285d6e3a54f783db1fb974ddd6a14a6
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 01:35:28 +0200

    GenericRepository

--- ECommerce.Domain/Contracts/IRepository/IGenericRepository.cs
+++ ECommerce.Domain/Contracts/IRepository/IGenericRepository.cs
@@ -8,6 +8,7 @@ using ECommerce.Domain.Entities;
 
 namespace ECommerce.Domain.Contracts.IRepository
 {
+    #region GenericRepository
     public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
         Task<IEnumerable<TEntity>> GetAllAsync();
@@ -19,4 +20,32 @@ namespace ECommerce.Domain.Contracts.IRepository
         Task<TEntity> GetByIdWithSpecificationAsync(ISpecifications<TEntity, TKey> specifications);
         Task<int> CountAsync(ISpecifications<TEntity, TKey> specifications);
     }
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public GenericRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity) => await _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public void DeleteAsync(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public void UpdateAsync(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllWithSpecificationAsync(ISpecifications<TEntity, TKey> specifications)
+        => await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEntity>(), specifications).ToListAsync();
+
+        public async Task<TEntity> GetByIdWithSpecificationAsync(ISpecifications<TEntity, TKey> specifications)
+        => await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEntity>(), specifications).FirstOrDefaultAsync();
+
+        public async Task<int> CountAsync(ISpecifications<TEntity, TKey> specifications)
+        => await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEntity>(), specifications).CountAsync();
+    }
+    #endregion
 }
